a08bb1070958ab42f5027eaeca44dea52f31bc4b
nganho <nganho2251@gmail.com>
2023-12-11 18:01:00 +0700

    change name DATE & TIME Table

--- SCRIPT/DATE&TIME table in NDS.sql
+++ SCRIPT/DATE&TIME table in NDS.sql
@@ -1,16 +1,28 @@
-﻿GO
-USE [BI07_NDS]
-GO
+﻿
+CREATE TABLE DATE_MASTER(
+	[DateKey] [varchar](8) NOT NULL,
+	[Day] [int] NULL,
+	[Month] [int] NULL,
+	[Year] [int] NULL,
+PRIMARY KEY (DateKey)
+)
 
-update DATE
-set Day = convert(int, SUBSTRING(DateKey, 7, 2)), Month = convert(int, SUBSTRING(DateKey, 5, 2)), Year = convert(int, SUBSTRING(DateKey, 1, 4));
+CREATE TABLE TIME_MASTER(
+	[TimeKey] [varchar](4) NOT NULL,
+	[Hour] [int] NULL,
+	[Minute] [int] NULL,
+PRIMARY KEY (TimeKey)
+)
 
 
-insert into DATE(DateKey) SELECT TOP (DATEDIFF(DAY, '2019-01-01', '2019-03-31') + 1)
-    CONVERT(VARCHAR(8), DATEADD(DAY, ROW_NUMBER() OVER (ORDER BY (SELECT NULL)) - 1, '2019-01-01'), 112) AS MaDate
+
+INSERT INTO DATE_MASTER(DateKey)
+SELECT TOP (DATEDIFF(DAY, '2019-01-01', '2019-03-31') + 1)
+    CONVERT(VARCHAR(8), DATEADD(DAY, ROW_NUMBER() OVER (ORDER BY (SELECT NULL)) - 1, '2019-01-01'), 112) AS DateKey
 FROM master.dbo.spt_values;
 
-update DATE
+
+update DATE_MASTER
 set Day = convert(int, SUBSTRING(DateKey, 7, 2)), Month = convert(int, SUBSTRING(DateKey, 5, 2)), Year = convert(int, SUBSTRING(DateKey, 1, 4));
 
 
@@ -20,7 +32,7 @@ while @Hour <= 23
 begin
     while @Minute <= 59
     begin
-        insert into TIME (TimeKey, Hour, Minute)
+        insert into TIME_MASTER (TimeKey, Hour, Minute)
         values (
             RIGHT('00' + CAST(@Hour AS VARCHAR(2)), 2) + RIGHT('00' + CAST(@Minute AS VARCHAR(2)), 2),
             @Hour,
